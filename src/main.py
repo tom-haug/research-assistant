ca6ca0878aaa9f1edb2dfb9fb2270358ca305ef6
Tom Haug <38592049+tom-haug@users.noreply.github.com>
2024-03-13 14:16:34 +0000

    refactor chain

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from web_researcher import generate_report_from_question
 
 if __name__ == "__main__":
-  output = generate_report_from_question("What is the origin of the universe?");
+  output = generate_report_from_question("What is langsmith?");
   
   print(output)
